b0928cb4c584896ccd45afbcfe4b2f380161bcce
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-04-01 21:45:58 -0600

    databased seeded properly

--- server/utils/seeds.js
+++ server/utils/seeds.js
@@ -3,44 +3,19 @@ const path = require('path');
 const Category = require('../models/Categories');
 const Question = require('../models/Questions');
 
-function readJSONFilesFromDirectory() {
-  const directory = path.dirname(__filename);
-  const files = fs.readdirSync(directory);
-  return files
-    .filter(file => file.endsWith('.json'))
-    .map(file => {
-      const filePath = path.join(directory, file);
-      const fileContent = fs.readFileSync(filePath, 'utf8');
-      return JSON.parse(fileContent);
+const connectDB = require('../config/connection');
+const animalJson = require('./animalQuestions.json');
+const deathJson = require('./deathQuestions.json');
+
+    connectDB.once('open', async () => {
+
+      await Question.insertMany(animalJson); 
+      await Question.insertMany(deathJson);
+
+      console.log('Database seeded successfully');
+      process.exit(0);
     });
-}
 
-async function seedDataFromJSON() {
-  try {
-    const categories = readJSONFilesFromDirectory();
-
-    for (const category of categories) {
-      const { name, questions } = category;
-
-      const seededCategory = await Category.findOneAndUpdate(
-        { name }, 
-        { name }, 
-        { upsert: true, new: true } 
-      );
-
-      if (questions && questions.length > 0) {
-        const questionObjects = questions.map(question => ({
-          ...question,
-          category: seededCategory._id
-        }));
-        await Question.insertMany(questionObjects);
-      }
-    }
-
-    console.log('Database seeded successfully');
-  } catch (error) {
-    console.error('Error seeding database:', error);
-  }
-}
+   
 
 

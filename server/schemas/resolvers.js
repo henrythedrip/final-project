f5f88081279f7a4e1e7bb71d7f911fff55e4e1f6
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-04-01 19:39:11 -0600

    added resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,29 @@
 const { Question, User, Category } = require('../models');
 
+const resolvers = {
+    Query: {
+      question: async (_, { questionId }) => {
+        return await Question.findById(questionId);
+      },
+      questionsByCategory: async (_, { categoryId }) => {
+        return await Question.find({ category: categoryId });
+      },
+      categories: async () => {
+        return await Category.find();
+      },
+    },
+    Category: {
+      questions: async (category) => {
+        return await Question.find({ category: category._id });
+      },
+    },
+    Question: {
+      category: async (question) => {
+        return await Category.findById(question.category);
+      },
+    },
+  };
+
 module.exports = {
     Question,
     User,

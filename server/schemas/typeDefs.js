e9563b473bf67dd42938d2002d9b06ef4c6730f9
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-04-01 19:39:24 -0600

    added more to the typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,38 @@
-const typeDefs = `
-type Category {
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Category {
     _id: ID
     name: String
-    setOfQuestions: [Question]
-}
+    questions: [Question]
+  }
 
-type Question {
+  type Question {
     _id: ID
     questionBody: String
     answer: Boolean
-    category: [Category]
-}
+    category: Category
+  }
 
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     password: String
-}
-`
+  }
+
+  type Query {
+    question(questionId: ID!): Question
+    questionsByCategory(categoryId: ID!, difficulty: String): [Question]!
+    categories: [Category]!
+  }
+
+  type Mutation {
+    addQuestion(questionBody: String!, answer: Boolean!, categoryId: ID!): Question
+    removeQuestion(questionId: ID!): Question
+    addCategory(name: String!): Category
+    removeCategory(categoryId: ID!): Category
+  }
+`;
+
+module.exports = typeDefs;

709525ba24ff05ebe97df12dc0d3c24783dfbeb3
Capti <captianbeau@gmail.com>
2024-04-02 16:04:51 -0600

    removed unused files

--- server/routes/api/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-// this is a smoke test that can be deleted later
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-module.exports = router
